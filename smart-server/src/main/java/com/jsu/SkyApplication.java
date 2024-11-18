34e693fc9a871721d5c7522c96c2b361c5467020
XuanFlourish <2621319950@qq.com>
2024-11-18 17:26:17 +0800

    first commit

---
+++ smart-server/src/main/java/com/jsu/SkyApplication.java
@@ -0,0 +1,34 @@
+package com.jsu;
+
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.transaction.annotation.EnableTransactionManagement;
+
+@SpringBootApplication
+@Slf4j
+public class SkyApplication {
+    public static void main(String[] args) {
+        SpringApplication.run(SkyApplication.class, args);
+        System.out.println("                   _ooOoo_");
+        System.out.println("                  o8888888o");
+        System.out.println("                  88\" . \"88");
+        System.out.println("                  (| -_- |)");
+        System.out.println("                  O\\  =  /O");
+        System.out.println("               ____/`---'\\____");
+        System.out.println("             .'  \\\\|     |//  `.");
+        System.out.println("            /  \\\\|||  :  |||//  \\");
+        System.out.println("           /  _||||| -:- |||||-  \\");
+        System.out.println("           |   | \\\\\\  -  /// |   |");
+        System.out.println("           | \\_|  ''\\---/''  |   |");
+        System.out.println("           \\  .-\\__  `-`  ___/-. /");
+        System.out.println("         ___`. .'  /--.--\\  `. . __");
+        System.out.println( "      .\"\" '<  `.___\\_<|>_/___.'  >'\"\".");
+        System.out.println("     | | :  `- \\`.;`\\ _ /`;.`/ - ` : | |");
+        System.out.println("     \\  \\ `-.   \\_ __\\ /__ _/   .-` /  /");
+        System.out.println("======`-.____`-.___\\_____/___.-`____.-'======");
+        System.out.println("                   `=---='");
+        System.out.println("^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^");
+        System.out.println("               佛祖保佑       永无BUG");
+    }
+}
